3fcb2b56ea0e087bdc8ad6e1c5b287e17535eaca
PN\nhi.nguyen <nhi.nguyen@student.passerelesnumeriques.org>
2018-03-05 11:08:55 +0700

    Add web file

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -2,6 +2,7 @@
 <html>
     <head>
         <title>Food Store</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
         <script src="js/jquery.min.js"></script>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/products', 'HomeController@index1')->name('products');
+Route::get('/product_details', 'HomeController@index2')->name('product_details');
+Route::get('/cart', 'HomeController@index3')->name('cart');
+Route::resource('admin/products', 'AdminProductsController', array('as' => 'admin'));
+Route::resource('admin/categories', 'AdminCategoriesController', array('as' => 'admin'));
