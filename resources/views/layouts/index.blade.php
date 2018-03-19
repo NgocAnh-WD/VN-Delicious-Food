2c1eb034a5ac7bcd5ebbec25ddc78b384cbd5f8e
PN\thuong.nguyen <Thuong.nguyen@student.passerellesnumeriques.org>
2018-03-19 10:56:00 +0700

    Edit form Register

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -29,26 +29,66 @@
     </head>
     <body> 
         <!--header-->	
-        <div class="top_bg">
+        <div id="app">
+        <nav class="navbar navbar-default navbar-static-top">
             <div class="container">
-                <div class="header_top-sec">
-                    <div class="top_right">
-                        <ul>
-                            <li><a href="#">help</a></li>|
-                            <li><a href="contact.html">Contact</a></li>|
-                            <li><a href="login.html">Track Order</a></li>
-                        </ul>
-                    </div>
-                    <div class="top_left">
-                        <ul>
-                            <li class="top_link">Email:<a href="mailto:info@example.com">mail@example.com</a></li>|
-                            <li class="top_link"><a href="login.html">My Account</a></li>					
-                        </ul>
-                    </div>
-                    <div class="clearfix"> </div>
+                <div class="navbar-header">
+
+                    <!-- Collapsed Hamburger -->
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                        <span class="sr-only">Toggle Navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+
+                    <!-- Branding Image -->
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        {{ config('app.name', 'Laravel') }}
+                    </a>
+                </div>
+
+                <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="nav navbar-nav">
+                        &nbsp;
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="nav navbar-nav navbar-right">
+                        <!-- Authentication Links -->
+                        @if (Auth::guest())
+                            <li><a href="{{ route('login') }}">Login</a></li>
+                            <li><a href="{{ route('register') }}">Register</a></li>
+                        @else
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    {{ Auth::user()->username }} <span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu" role="menu">
+                                    <li>
+                                        <a href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            Logout
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                        @endif
+                    </ul>
                 </div>
             </div>
-        </div>
+        </nav>
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
         <div class="header-top">
             <div class="header-bottom">
                 <div class="container">			
