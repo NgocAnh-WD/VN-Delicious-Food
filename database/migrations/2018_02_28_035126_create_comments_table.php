1760aea1eaddfed2956e58f6d24e5911a1bd2083
PN\anh.nguyen <anh.nguyen@student.passerellesnumeriques.org>
2018-03-30 10:02:10 +0700

    Chạy lại migration comment với, migration không đúng

--- database/migrations/2018_02_28_035126_create_comments_table.php
+++ database/migrations/2018_02_28_035126_create_comments_table.php
@@ -4,28 +4,27 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCommentsTable extends Migration
-{
+class CreateCommentsTable extends Migration {
+
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned()->index();
             $table->integer('product_id')->unsigned()->index();
-            $table->integer('parent_id')->default(null);
-            $table->string('title');
+            $table->integer('parent_id')->default(0);
+            $table->string('title')->default(null);
             $table->string('content');
             $table->integer('is_delete')->default(0);
             $table->timestamps();
-            
+
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
-        });           
+        });
     }
 
     /**
@@ -33,8 +32,8 @@ class CreateCommentsTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('comments');
     }
+
 }
