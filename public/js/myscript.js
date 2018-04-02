48a150c8b4baea8af139794f854ca8df461ff417
PN\nhi.nguyen <nhi.nguyen@student.passerelesnumeriques.org>
2018-04-02 11:38:36 +0700

    Edit Path URL on Shopping Cart

--- public/js/myscript.js
+++ public/js/myscript.js
@@ -1,5 +1,5 @@
 function close_cart(id) {
-    var url = 'http://localhost/php_laravel_project/public/product/delete' + '/' + id;
+    var url = getSitePublicUrl()+'/product/delete' + '/' + id;
     var check = confirm('Bạn chắc chắn muốn xóa sản phẩm này chứ');
     if (check == 1) {
         window.location.href = url;
@@ -8,17 +8,21 @@ function close_cart(id) {
     }
 }
 function pluscart(id, quantity) {
-    var url = 'http://localhost/php_laravel_project/public/addtocart' + '/' + id;
+    var url =getSitePublicUrl()+'/addtocart' + '/' + id;
     window.location.href = url;
 }
 function subtractcart(id, quantity) {
     var qty = quantity;
     if (qty > 1) {
-        var url = 'http://localhost/php_laravel_project/public/product/deductbyone' + '/' + id;
+        var url =getSitePublicUrl()+'/product/deductbyone' + '/' + id;
         window.location.href = url;
     } else {
         close_cart(id);
     }
 }
 
-
+function getSitePublicUrl(){
+    var url = '';
+    var url ='http://'+window.location.host +  window.location.pathname.substring(0, window.location.pathname.lastIndexOf('/')); 
+    return url;
+}
