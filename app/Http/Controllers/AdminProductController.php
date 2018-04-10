ab5d7384a03478254f015c83664aeb1fc9a6b326
DESKTOP-18M8LVS <0935640042.nn@gmail.com>
2018-04-10 23:57:31 +0700

    edit AdminProductsController and create products

--- app/Http/Controllers/AdminProductController.php
+++ app/Http/Controllers/AdminProductController.php
@@ -35,7 +35,7 @@ class AdminProductController extends Controller {
      * @return \Illuminate\Http\Response
      */
     public function create() {
-        $child_categories = Category::pluck('name', 'id')->all();
+        $child_categories = Category::where('parent_id', '!=', 0)->get();
         return view('admin.products.create', compact('child_categories'));
     }
 
@@ -46,51 +46,115 @@ class AdminProductController extends Controller {
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request) {
-        $input = $request->all();
+        $this->validate($request, [
+            'name' => 'required|min:2',
+            'link_image' => 'required',
+            'description' => 'required',
+            'quality' => 'required',
+            'price' => 'required',
+            'quantity' => 'required',
+                ], [
+            'name.required' => 'Vui lòng nhập tên sản phẩm.',
+            'link_image.required' => 'Vui lòng chọn ảnh chính.',
+            'description.required' => 'Vui lòng mô tả chi tiết.',
+            'quality.required' => 'Vui lòng kiểm tra dữ liệu.',
+            'quantity.required' => 'Vui lòng kiểm tra dữ liệu.',
+            'price.required' => 'Vui lòng kiểm tra dữ liệu.'
+        ]);
+        DB::beginTransaction();
+        try {
+            $input = $request->all();
+            $input['name'] = $request->get('name');
+            $input['category_id'] = $request->get('category_id');
+            $input['description'] = $request->get('description');
+            $input['is_delete'] = 0;
+            $product = Product::create($input);
+            if($product != null){
+                $input['product_id'] = $product->id;
+                $input['size'] = $request->get('size');
+                $input['quality'] = $request->get('quality');
+                $input['quantity'] = $request->get('quantity');
+                $input['price'] = $request->get('price');
+                $input['is_price'] = 0;
+                $price_size = PriceSizes::create($input);
+                if ($file = $request->file('link_image')) {
+                    $year = date('Y');
+                    $month = date('m');
 
-//        $product = new Product();
-//        $product->create($input);
-        $product = Product::create($input);
-        $input['product_id'] = $product->id;
-        $input['size'] = $request->get('size');
-        $input['quality'] = $request->get('quality');
-        $input['price'] = $request->get('price');
-        $input['quantity'] = $request->get('quantity');
-        $input['is_delete'] = 0;
-        PriceSizes::create($input);
+                    $day = date('d');
+                    $sub_folder = $year . '/' . $month . '/' . $day . '/';
+                    $upload_url = 'images/' . $sub_folder;
 
-        if ($file = $request->file('link_image')) {
-            $year = date('Y');
-            $month = date('m');
+                    if (!File::exists(public_path() . '/' . $upload_url)) {
+                        File::makeDirectory(public_path() . '/' . $upload_url, 0777, true);
+                    }
+                    $name = time() . $file->getClientOriginalName();
+                    $file->move($upload_url, $name);
+                    $images = Image::create(['link_image' => $upload_url . $name, 'product_id' => $product->id, 'is_delete' => 0, 'is_thumbnail' => 1]);
+                }
+                if ($file = $request->file('image')) {
+                    $year = date('Y');
+                    $month = date('m');
 
-            $day = date('d');
-            $sub_folder = $year . '/' . $month . '/' . $day . '/';
-            $upload_url = 'images/' . $sub_folder;
+                    $day = date('d');
+                    $sub_folder = $year . '/' . $month . '/' . $day . '/';
+                    $upload_url = 'images/' . $sub_folder;
 
-            if (!File::exists(public_path() . '/' . $upload_url)) {
-                File::makeDirectory(public_path() . '/' . $upload_url, 0777, true);
+                    if (!File::exists(public_path() . '/' . $upload_url)) {
+                        File::makeDirectory(public_path() . '/' . $upload_url, 0777, true);
+                    }
+                    $name = time() . $file->getClientOriginalName();
+                    $file->move($upload_url, $name);
+                    $images = Image::create(['link_image' => $upload_url . $name, 'product_id' => $product->id, 'is_delete' => 0, 'is_thumbnail' => 0]);
+                }
             }
-            $name = time() . $file->getClientOriginalName();
-            $file->move($upload_url, $name);
-            $images = Image::create(['link_image' => $upload_url . $name, 'product_id' => $product->id, 'is_delete' => 0, 'is_thumbnail' => 1]);
+            DB::commit();
+        } catch (Exception $ex) {
+            
         }
-        if ($file = $request->file('image')) {
-            $year = date('Y');
-            $month = date('m');
-
-            $day = date('d');
-            $sub_folder = $year . '/' . $month . '/' . $day . '/';
-            $upload_url = 'images/' . $sub_folder;
-
-            if (!File::exists(public_path() . '/' . $upload_url)) {
-                File::makeDirectory(public_path() . '/' . $upload_url, 0777, true);
-            }
-            $name = time() . $file->getClientOriginalName();
-            $file->move($upload_url, $name);
-            $images = Image::create(['link_image' => $upload_url . $name, 'product_id' => $product->id, 'is_delete' => 0, 'is_thumbnail' => 0]);
-        }
-
-        return redirect('/admin/products');
+        return redirect('/admin/products')->with('create_product', 'Bạn đã thêm sản phẩm thành công');
+//        $input = $request->all();
+//        $product = Product::create($input);
+//        $input['product_id'] = $product->id;
+//        $input['size'] = $request->get('size');
+//        $input['quality'] = $request->get('quality');
+//        $input['price'] = $request->get('price');
+//        $input['quantity'] = $request->get('quantity');
+//        $input['is_delete'] = 0;
+//        PriceSizes::create($input);
+//
+//        if ($file = $request->file('link_image')) {
+//            $year = date('Y');
+//            $month = date('m');
+//
+//            $day = date('d');
+//            $sub_folder = $year . '/' . $month . '/' . $day . '/';
+//            $upload_url = 'images/' . $sub_folder;
+//
+//            if (!File::exists(public_path() . '/' . $upload_url)) {
+//                File::makeDirectory(public_path() . '/' . $upload_url, 0777, true);
+//            }
+//            $name = time() . $file->getClientOriginalName();
+//            $file->move($upload_url, $name);
+//            $images = Image::create(['link_image' => $upload_url . $name, 'product_id' => $product->id, 'is_delete' => 0, 'is_thumbnail' => 1]);
+//        }
+//        if ($file = $request->file('image')) {
+//            $year = date('Y');
+//            $month = date('m');
+//
+//            $day = date('d');
+//            $sub_folder = $year . '/' . $month . '/' . $day . '/';
+//            $upload_url = 'images/' . $sub_folder;
+//
+//            if (!File::exists(public_path() . '/' . $upload_url)) {
+//                File::makeDirectory(public_path() . '/' . $upload_url, 0777, true);
+//            }
+//            $name = time() . $file->getClientOriginalName();
+//            $file->move($upload_url, $name);
+//            $images = Image::create(['link_image' => $upload_url . $name, 'product_id' => $product->id, 'is_delete' => 0, 'is_thumbnail' => 0]);
+//        }
+//
+//        return redirect('/admin/products');
     }
 
     /**
@@ -187,8 +251,9 @@ class AdminProductController extends Controller {
         return redirect('/admin/products');
     }
 
-    public function getImage($id){
-        $images = Image::select('id', 'link_image')->where('product_id',$id)->get();
+    public function getImage($id) {
+        $images = Image::select('id', 'link_image')->where('product_id', $id)->get();
         return view('/admin/products', compact('images'));
     }
+
 }
--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -1,89 +1,85 @@
 @extends('layouts.admin')
 
 @section('content')
-    @include('includes.tinyeditor')
+@include('includes.tinyeditor')
 
-    <h1>Create Products</h1>
+<h1>Create Products</h1>
 
-    <div class="container">
-         
-        <form action="{{ route('admin.products.store') }}" method="post" enctype='multipart/form-data'>
-            <input type="hidden" name="_token" value="{{ csrf_token() }}"/>
+<div class="container">
 
-        
+    <form action="{{ route('admin.products.store') }}" method="post" enctype='multipart/form-data'>
+        <input type="hidden" name="_token" value="{{ csrf_token() }}"/>
 
-            <div class="form-group {{ $errors->has('name') ? 'has-error' : '' }}">
-                <label for="name">Name:</label>
-                <input type="text" id="name" name="name" class="form-control" placeholder="Enter product name" value="{{ old('name') }}" required>
-                <span class="text-danger">{{ $errors->first('name') }}</span>
-            </div>    
-            
-            <div class=" form-group {{ $errors->has('category_id') ? 'has-error' : '' }}">
-                    <label for="category_id">Category:</label>
-                    <select class="form-control" id="category_id" name="category_id" >
-                       @foreach ($child_categories as $id => $name  )
-                       <option value="{{$id}}">{{$name}}</option>
-                        @endforeach   
-                     </select>                
-                    <span class="text-danger">{{ $errors->first('category_id') }}</span>
-            </div>  
-            
-            
-            <div class="  form-group {{ $errors->has('link_image') ? 'has-error' : '' }}">
-                <label for="link_image">Thumbnail:</label>
-                <input type="file" id="link_image" name="link_image" class="form-control" value="{{ old('link_image') }}">
-                <span class="text-danger">{{ $errors->first('link_image') }}</span>
-           </div> 
-            <div class="  form-group {{ $errors->has('image') ? 'has-error' : '' }}">
-                <label for="image">Image:</label>
-                <input type="file" id="link_image" name="image" class="form-control" value="{{ old('image') }}">
-                <span class="text-danger">{{ $errors->first('image') }}</span>
-           </div> 
-            
-           <div class=" form-group {{ $errors->has('description') ? 'has-error' : '' }}">
-                    <label for="body">Descriptions:</label>
-                     <textarea class="form-control" rows="5" id="body" name="description">{{ old('description') }}</textarea>
-                    <span class="text-danger">{{ $errors->first('description') }}</span>
-            </div> 
-            
-            <div class=" form-group {{ $errors->has('size') ? 'has-error' : '' }}">
-                    <label for="size">Size:</label>
-                     <select id="size" class="form-control" name="size" required>
-                                    <option value="Lớn">Lớn</option>
-                                    <option value="Trung">Trung</option>
-                                    <option value="Nhỏ">Nhỏ</option>
-                     </select>
-                    <span class="text-danger">{{ $errors->first('size') }}</span>
-            </div> 
-            
-            <div class=" form-group {{ $errors->has('quality') ? 'has-error' : '' }}">
-                    <label for="quality">Quality:</label>
-                    <input type="text" id="title" name="quality" class="form-control" placeholder="" value="{{ old('quality') }}">
-                    <span class="text-danger">{{ $errors->first('quality') }}</span>
-            </div>  
-            
-            <div class=" form-group {{ $errors->has('price') ? 'has-error' : '' }}">
-                    <label for="price">Price:</label>
-                    <input type="number" id="title" name="price" class="form-control" placeholder="" value="{{ old('price') }}">
-                    <span class="text-danger">{{ $errors->first('price') }}</span>
-            </div>
-            
-             <div class=" form-group {{ $errors->has('quantity') ? 'has-error' : '' }}">
-                    <label for="quantity">Quantity:</label>
-                    <input type="number" id="title" name="quantity" class="form-control" placeholder="" value="{{ old('quantity') }}">
-                    <span class="text-danger">{{ $errors->first('quantity') }}</span>
-            </div>
-         
-            
-            <div class="form-group">
-                <input type="submit" class="btn btn-success" value="Create Product" />
-            </div>        
-    </div>
-    <div class="col-md-1"></div>
 
- </form>  
 
-    <div class="row">
-        @include('includes.form_error')
-    </div>
+        <div class="form-group {{ $errors->has('name') ? 'has-error' : '' }}">
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" class="form-control" placeholder="Enter product name" value="{{ old('name') }}" required>
+            <span class="text-danger">{{ $errors->first('name') }}</span>
+        </div>    
+
+        <div class=" form-group {{ $errors->has('category_id') ? 'has-error' : '' }}">
+            <label for="category_id">Category:</label>
+            <select class="form-control" id="category_id" name="category_id" >
+                @foreach ($child_categories as $key => $name  )
+                <option value="{{$name->id}}">{{$name->name}}</option>
+                @endforeach   
+            </select>                
+            <span class="text-danger">{{ $errors->first('category_id') }}</span>
+        </div>  
+
+
+        <div class="  form-group {{ $errors->has('link_image') ? 'has-error' : '' }}">
+            <label for="link_image">Ảnh chính:</label>
+            <input type="file" id="link_image" name="link_image" class="form-control" value="{{ old('link_image') }}">
+            <span class="text-danger">{{ $errors->first('link_image') }}</span>
+        </div> 
+        <div class="  form-group {{ $errors->has('image') ? 'has-error' : '' }}">
+            <label for="image">Ảnh phụ:</label>
+            <input type="file" id="image" name="image" class="form-control" value="{{ old('image') }}">
+            <span class="text-danger">{{ $errors->first('image') }}</span>
+        </div> 
+
+        <div class=" form-group {{ $errors->has('description') ? 'has-error' : '' }}">
+            <label for="body">Descriptions:</label>
+            <textarea class="form-control" rows="5" id="body" name="description">{{ old('description') }}</textarea>
+            <span class="text-danger">{{ $errors->first('description') }}</span>
+        </div> 
+
+        <div class=" form-group {{ $errors->has('size') ? 'has-error' : '' }}">
+            <label for="size">Size:</label>
+            <select id="size" class="form-control" name="size" required>
+                <option value="l">Lớn</option>
+                <option value="m">Trung</option>
+                <option value="s">Nhỏ</option>
+            </select>
+            <span class="text-danger">{{ $errors->first('size') }}</span>
+        </div> 
+
+        <div class=" form-group {{ $errors->has('quality') ? 'has-error' : '' }}">
+            <label for="quality">Quality:</label>
+            <input type="text" id="title" name="quality" class="form-control" placeholder="" value="{{ old('quality') }}">
+            <span class="text-danger">{{ $errors->first('quality') }}</span>
+        </div>  
+
+        <div class=" form-group {{ $errors->has('price') ? 'has-error' : '' }}">
+            <label for="price">Price:</label>
+            <input type="number" id="title" name="price" class="form-control" placeholder="" value="{{ old('price') }}">
+            <span class="text-danger">{{ $errors->first('price') }}</span>
+        </div>
+
+        <div class=" form-group {{ $errors->has('quantity') ? 'has-error' : '' }}">
+            <label for="quantity">Quantity:</label>
+            <input type="number" id="title" name="quantity" class="form-control" placeholder="" value="{{ old('quantity') }}">
+            <span class="text-danger">{{ $errors->first('quantity') }}</span>
+        </div>
+
+
+        <div class="form-group">
+            <input type="submit" class="btn btn-success" value="Create Product" />
+        </div>        
+</div>
+<div class="col-md-1"></div>
+
+</form>  
 @stop
