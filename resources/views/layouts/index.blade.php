27f2d9171908c4e6fc917c62187a28a24b226bd6
PN\anh.nguyen <anh.nguyen@student.passerellesnumeriques.org>
2018-04-19 14:54:45 +0700

    trang Index

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -11,8 +11,6 @@
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="keywords" content="Wedding Store Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
               Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
-<!--        <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>-->
-
         <script type="text/javascript" src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <script src="{{asset('js/simpleCart.min.js')}}"></script>
@@ -20,6 +18,8 @@
         <link href="{{asset('css/style1.css')}}" rel="stylesheet">        
         <link href="{{asset('css/form.css')}}" rel="stylesheet" type="text/css" media="all" />
         <link href="{{asset('css/search_name_product.css')}}" rel="stylesheet">
+        <link href="{{asset('css/contact.css')}}" rel="stylesheet" /> 
+        <link href="//fonts.googleapis.com/css?family=Tangerine:400,700" rel="stylesheet">
         <script type="text/javascript" src="{{asset('js/memenu.js')}}"></script>
         <script type="text/javascript" src="{{asset('js/myscript.js')}}"></script>
         <script type="text/javascript" src="{{asset('js/searchPrice.js')}}"></script>
@@ -29,66 +29,103 @@ var GlobleVariable = [];
 GlobleVariable.app_url = "<?php echo env('APP_URL'); ?>";
         </script>	
         <!-- /start menu -->      
+
     </head>
     <body> 
-        <div class="header-top">
-            <div class="header-bottom">
-                <div class="container">			
-                    <div class="logo">
-                        <a href="index.html"><h1>5PANDAS</h1></a>
-                    </div>                   
-                </div>                    
+        <div class="banner inner-bg-w3" id="home">
+            <div class="banner-top">
+                <div class="social-bnr-agileits">
+                    <ul>
+                        <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>					
+                    </ul>
+                </div>
+                <div class="contact-bnr-w3-agile">
+                    <ul>
+                        <li><i class="fa fa-envelope" aria-hidden="true"></i><a href="5Pandas@passerellesnumeriques.org">5Pandas@passerellesnumeriques.org</a></li>
+                        <li><i class="fa fa-phone" aria-hidden="true"></i>0946777888</li>	
+                        <li>
+                            <div class="search">
+                                <input class="search_box" type="checkbox" id="search_box">
+                                <label class="icon-search" for="search_box"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></label>
+                                <div class="search_form">
+                                    <form action="{{url('/search')}}" method="GET" role="search">
+                                        <input type="text" name="key_search" id="key_search_button" placeholder="Search..">
+                                        <input type="submit" value="Search" />
+                                    </form>
+                                </div>
+                            </div> 
+                        </li>
+                    </ul>
+                </div>
+                <div class="clearfix"></div>
             </div>
+            <header>
+                <div class="container">
+                    <div class="w3_navigation">
+                        <nav class="navbar navbar-default">
+                            <div class="navbar-header navbar-left">
+                                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                                    <span class="sr-only">Toggle navigation</span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </button>
+                                <div class="w3_navigation_pos">
+                                    <h1><a href="{{asset('/home')}}"><span>5Pandas</span></a></h1>
+                                </div>
+                            </div>
+                            <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+                                <nav class="menu menu--miranda">
+                                    <ul class="nav navbar-nav menu__list">
+                                        <li class="menu__item"><a href="{{asset('/home')}}" class="menu__link">Home</a></li>
+                                        <li class="menu__item"><a href="{{asset('/about')}}" class=" menu__link">About Us</a></li>
+                                        <li class="menu__item"><a href="{{asset('/products')}}" class=" menu__link">Products</a></li>
+                                        <li class="menu__item"><a href="{{asset('/contact')}}" class=" menu__link">Contact</a></li>
+
+                                        @guest
+                                        <li class="dropdown menu__item"><a href="{{ route('login') }}"><b>Login/Register</b></a></li>
+                                        @else
+                                        <li class="dropdown" style="float: right; margin-top: 30px; padding-right: px">
+                                            <button class="dropbtn">{{ Auth::user()->username }}<span class="caret"></span></button>
+                                            <div class="dropdown-content">
+                                                <a href="{{ route('logout')}}"
+                                                   onclick="event.preventDefault();
+                                                           document.getElementById('logout-form').submit();">
+                                                    Logout                                       
+                                                </a>
+                                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                                    {{ csrf_field() }}
+                                                </form>
+                                                <a href="{{asset('/profile')}}">View profile</a>
+                                                <a href="{{asset('/profile')}}">View Orders</a>
+                                            </div>
+                                        </li>
+                                        @endguest  
+
+                                    </ul>
+                                </nav>
+                            </div>
+                        </nav>	
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+            </header>
         </div>
-        <div class="top-nav">
-            <ul class="memenu skyblue">
-                <li class="grid"><a href="{{asset('/home')}}">Home</li>
-                <li  class="grid"><a href="{{asset('/products')}}"><b>Food</b></a></li>
-                <li>
-                    <div class="search">
-                        <input class="search_box" type="checkbox" id="search_box">
-                        <label class="icon-search" for="search_box"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></label>
-                        <div class="search_form">
-                            <form action="{{url('/search')}}" method="GET" role="search">
-                                <input type="text" name="key_search" id="key_search_button" placeholder="Search..."">
-                                <input type="submit" value="Search">
-                            </form>
-                        </div>
-                    </div>
-                </li>
 
-                @guest
-                <li class="grid"><a href="{{ route('login') }}"><b>Login/Register</b></a></li>
-                @else
-                <li class="dropdown" style="float: right; margin-top: 30px; padding-right: px">
-                    <button class="dropbtn">{{ Auth::user()->username }}<span class="caret"></span></button>
-                    <div class="dropdown-content">
-                        <a href="{{ route('logout')}}"
-                           onclick="event.preventDefault();
-                                               document.getElementById('logout-form').submit();">
-                            Logout                                       
-                        </a>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            {{ csrf_field() }}
-                        </form>
-                        <a href="{{asset('/profile')}}">View profile</a>
-                        <a href="{{asset('/profile')}}">View Orders</a>
-                    </div>
-                </li>
-                @endguest    
-            </ul>
-            <div class="clearfix"></div>   
-            <div class="cart box_1 test">
+        <div class="cart box_1 test">
 
-                <a href="{{ url('shoppingcart') }}">
-                    <h3> <div class="total">
-                            <span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"><span class="badge">{{ Session::has('cart') ? Session::get('cart')->totalQty : '' }}</span></span>
-                            <div class="clearfix"></div>
-                        </div>
-                    </h3>
-                </a>
-            </div>
-        </div>	
+            <a href="{{ url('shoppingcart') }}">
+                <h3> <div class="total">
+                        <span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"><span class="badge">{{ Session::has('cart') ? Session::get('cart')->totalQty : '' }}</span></span>
+                        <div class="clearfix"></div>
+                    </div>
+                </h3>
+            </a>
+        </div>
+        <div class="clearfix"> </div>
         @yield('welcome')
         <!---->
         @yield('ads')
@@ -125,13 +162,13 @@ GlobleVariable.app_url = "<?php echo env('APP_URL'); ?>";
         <div class="footer">
             <div class="ftr-grids">
                 <div class="col-md-3 ftr-grid">
-                    <h4>Địa chỉ</h4>
+                    <h4 style="color: white;">Địa chỉ</h4>
                     <div class="maps">
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3834.1103360974585!2d108.24146331471736!3d16.05976319395768!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3142177f2ced6d8b%3A0xeac35f2960ca74a4!2zOTkgVMO0IEhp4bq_biBUaMOgbmgsIFBoxrDhu5tjIE3hu7ksIFPGoW4gVHLDoCwgxJDDoCBO4bq1bmcsIFZpZXRuYW0!5e0!3m2!1sen!2s!4v1523962542866" width="360" height="200" frameborder="0" style="border:1px; margin-top: 5px;" allowfullscreen></iframe>
                     </div>
                 </div>
                 <div class="col-md-6 ftr-grid">
-                    <h4>Giới thiệu</h4>
+                    <h4 style="color: white;">Giới thiệu</h4>
                     <div class="row">
                         <div class="col-md-4">
                             <img src="{{asset('images/cooker.jpg')}}" alt="Image" style="height: 120px; width: 190px; margin-left: 15px;"/>
@@ -145,7 +182,7 @@ GlobleVariable.app_url = "<?php echo env('APP_URL'); ?>";
                     </div>
                     <ul>
                         <li>
-                            <p style="text-align: center; margin-top: 5px;">5 Pandas là website chính thức của chuỗi cửa hàng thức ăn 4PANDAS nổi tiếng. Ngoài việc cung 
+                            <p style="text-align: center; margin-top: 5px; color: white;">5 Pandas là website chính thức của chuỗi cửa hàng thức ăn 4PANDAS nổi tiếng. Ngoài việc cung 
                                 cấp các món ăn ngon của cửa hàng đến thực khách. Chúng tôi còn liên kết với các chuỗi nhà hàng nổi
                                 tiếng khác để đem đến cho các bạn thật nhiều món ăn ngon miệng. Chất lượng hàng đầu, giá thành hợp lý.
                             </p>
@@ -154,23 +191,23 @@ GlobleVariable.app_url = "<?php echo env('APP_URL'); ?>";
                     </ul>
                 </div>
                 <div class="col-md-3 ftr-grid">
-                    <h4>Thông tin hỗ trợ</h4>
+                    <h4 style="color: white;">Thông tin hỗ trợ</h4>
                     <ul>                                   
                         <li><img src="{{asset('images/panda-icon.png')}}" alt="Image" style="height: 50px; width: 50px;"/></li>
-                        <li><p style="font-weight: bold;">Mentor: Mr.Ngo Ho (SEA DEV Company)</p></li>
-                        <li><p>Phone: 0236 3888 503</p></li>
-                        <li><p>Address: 99 Tô Hiến Thành, Phước Mỹ, Sơn Trà, Đà Nẵng</p></li>
-                        <li><p>Email: dant4pandas@gmail.com</p></li>
-                        <li><p>Hour: Opend 7:30 AM - 10:00 PM</p></li>					 
+                        <li><p style="font-weight: bold; color: white;">Mentor: Mr.Ngo Ho (SEA DEV Company)</p></li>
+                        <li><p style="color: white;">Phone: 0236 3888 503</p></li>
+                        <li><p style="color: white;">Address: 99 Tô Hiến Thành, Phước Mỹ, Sơn Trà, Đà Nẵng</p></li>
+                        <li><p style="color: white;">Email: dant4pandas@gmail.com</p></li>
+                        <li><p style="color: white;">Hour: Opend 7:30 AM - 10:00 PM</p></li>					 
                     </ul>
                 </div>
                 <div class="clearfix"></div>
             </div>
             <div class="copywrite">
                 <div class="container">
-                    <p style="text-align: center; margin-top: 35px;">Copyright Â© 2018 4 Pandas Store. All Rights Reserved | Design by <a href="#">4 DOGS</a></p>
+                    <p style="text-align: center; margin-top: 35px; color: white;">Copyright Â© 2018 5 Pandas Store. All Rights Reserved | Design by <a href="#">5 PANDAS</a></p>
                 </div>
-            </div> 
+            </div>
         </div>
     </body>
 
