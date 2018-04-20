f752e3ed917870bc74d4501f21cb8259614bf8d0
PN\thuong.nguyen <Thuong.nguyen@student.passerellesnumeriques.org>
2018-04-20 07:46:22 +0700

    register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -45,10 +45,10 @@ use RegistersUsers;
      */
     protected function validator(array $data) {
         return Validator::make($data, [
-                    'name' => 'required|string|max:255',
+                    'name'     => 'required|string|max:255',
                     'email'    => 'required|string|email|max:255|unique:users',
                     'password' => 'required|string|min:6|confirmed',
-                    'phone'    => 'required|integer|min:10|'
+                    'phone'    => 'string|min:10'
         ]);
     }
 
